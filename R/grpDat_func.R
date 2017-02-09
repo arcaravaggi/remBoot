fe04c96d730b5c71673513430e46fc739737e637
Anthony Caravaggi <arcaravaggi@users.noreply.github.com>
2017-02-09 12:38:05 +0000

    Removed from Help file
    Non user-facing functions. Script removed to omit from Help file.

--- R/grpDat_func.R
+++ R/grpDat_func.R
@@ -1,19 +1,3 @@
-#' Required for split_dat
-#' 
-#' Separate data into groups prior to bootstrapping. Called by split_dat.
-#' 
-#' @param df = the data frame
-#' @param x = group (site), derived from df
-#' @examples 
-#' 
-#' grpDat_func <- function(df, x){
-#' return(df[which(df[,1] == x), ])
-#' }
-#' 
-#' split_dat <- function(.df) {
-#'   lapply(unique(.df[[1]]), grpDat_func, df = .df)
-#' }
-
 grpDat_func <- function(df, x){
   return(df[which(df[,1] == x), ])
 }
--- R/res_dat.R
+++ R/res_dat.R
@@ -1,17 +1,3 @@
-#' Data splitting for grpSize
-#' 
-#' Use known row number to tell function how many data to resample in each iteration
-#' 
-#' @param .df = the data frame
-#' @examples 
-#' 
-#' res_dat <- function(.df) {
-#' tapply(.df[,4], .df[,1], length)
-#' }
-#' 
-#' grpSize <- res_dat(df)
-
-
 res_dat <- function(.df) {
   tapply(.df[,4], .df[,1], length)
 }
