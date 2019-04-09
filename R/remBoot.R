d0bbd9c6ed31d705a2f262b1ade713a577ca3b70
Anthony Caravaggi <arcarava@glam.ac.uk>
2019-04-09 14:07:43 +0100

    Update to generate measures of variance

--- R/remBoot.R
+++ R/remBoot.R
@@ -1,20 +1,20 @@
 #' Random Encounter Model bootstrapping for error terms
 #' 
-#' This function allows the automated calculation of variance for animal densities estimates derived from Random Encounter Modelling. 
-#'
+#' Preliminary update 17th August 2018
+#' remBoot is being incorporated into the RandEM package
+#' 
 #' The function assumes that the first 4 columns of the dataset contain: 
 #' 1) An identifying number for each survey location (e.g. 1, 2, 3)
 #' 2) The number of individuals of the focal species observed in each capture
 #' 3) The radial distance to the detected animal in each capture, given in metres
 #' 4) The angle of detection based on the location of the detected animal in each capture, given in radians
 #' 
-#' For an example of how to use remBoot, click (\href{http://htmlpreview.github.io/?https://github.com/arcaravaggi/remBoot/blob/master/vignettes/remBoot.html}{here}).
-#' 
+#'  
 #' @param .df (data frame)
 #' @param tm (numeric) The total number of hours all cameras were left in-situ at a focal site
 #' @param v (numeric) The distance travelled by the focal species in 24 hours, in kilometres
 #' @param nboots (numeric) The number of bootstrap iterations
-#' @param error_stat (text) Variance, given as confidence intervals (ci) or standard deviation (sd)
+#' @param error_stat (text) Variance, given as 95% confidence intervals (ci), 95% quantiles (quant), standard error (se) or standard deviation (sd)
 #' @keywords density, population, random encounter model, bootstrap
 #' @export
 #' @examples
@@ -37,6 +37,29 @@
 remBoot <- function(.df, tm, v, nboots, error_stat){
   ## Written by Kevin Keenan 2013 and Anthony Caravaggi 2016
   ## distributed under GPL3
+  bsD <- function(dat, tm, v){
+    bsDat <- dat[sample(1:nrow(dat), size = nrow(dat), replace = TRUE), ]
+    dOut <- rem(bsDat, tm, v)
+    return(dOut)
+  }
+  grpDat_func <- function(df, x){
+    return(df[which(df[,1] == x), ])
+  }
+  
+  split_dat <- function(.df) {
+    lapply(unique(.df[[1]]), grpDat_func, df = .df)
+  }
+  rem <- function(dat, tm, v){
+    # Calculate y: total captures
+    y <- sum(dat[,2])
+    # Calculate the first half of the equation above
+    trm1 <- y/tm
+    trm2 <- pi/((v*mean(dat[,3], 
+                        na.rm = TRUE)) * (2 + (mean(dat[,4], 
+                                                    na.rm = TRUE))))
+    # Calculate D
+    return(trm1 * trm2)
+  }
   grpDat <- split_dat(.df)
   remsD <- lapply(grpDat, function(x){
     d <- replicate(nboots, bsD(x, tm, v)) 
@@ -48,7 +71,19 @@ remBoot <- function(.df, tm, v, nboots, error_stat){
     names(resSD) <- levels(.df[[1]])
     res$sd <- c(res, resSD)
   }
-  if(is.element("ci", error_stat)){
+  if(is.element("se", error_stat)){
+    resSE <- mapply("/",lapply(remsD, sd),lapply(grpDat, function(x) sqrt(nrow(x))),SIMPLIFY = FALSE)
+    names(resSE) <- levels(.df[[1]])
+    res$se <- c(res, resSE)
+  }
+  if(is.element("ci", error_stat)){ #quantiles replaced with CI function
+    require(Rmisc)
+    resCI <- as.data.frame(t(sapply(remsD, CI, ci = 0.95)))
+    resCI$mean <- NULL
+    rownames(resCI) <- levels(.df[[1]])
+    res$ci <- resCI
+  }
+  if(is.element("quant", error_stat)){
     resCI <- t(sapply(remsD, quantile, probs = c(0.025, 0.975)))
     colnames(resCI) <- c("Lower", "Upper")
     rownames(resCI) <- levels(.df[[1]])
--- README.md
+++ README.md
@@ -1,8 +1,15 @@
-# remBoot: An R package for Random Encounter Modelling
-Anthony Caravaggi  
+---
+title: "remBoot: An R package for Random Encounter Modelling"
+author: "Anthony Caravaggi"
+output:  
+      html_document:  
+        keep_md: TRUE 
+---
 
 
 remBoot is an implementation of the Random Encounter Model (REM) developed by Rowcliffe _et al._ (2008). The REM is based on brownian motion and allows the estimation of animal densities for a given survey area. The REM is particularly useful in that species do not need to be marked or exhibit individual markings for estimates to be calculated. This package contains a number of functions which allow the user to calculate densities for one or more sites and bootstrap their data to calculate variance.   
+
+The package was updated on th 9th of April 2019 to generate four measures of variance: standard deviation; standard error; 95% confidence intervals; quantiles.
 
 # Installation
 
@@ -45,19 +52,19 @@ remsSD
 
 ```
 ## [[1]]
-## [1] 0.1198064
+## [1] 0.1120678
 ## 
 ## [[2]]
-## [1] 0.2090603
+## [1] 0.2166288
 ## 
 ## [[3]]
-## [1] 0.272623
+## [1] 0.2980642
 ## 
 ## [[4]]
-## [1] 0.1945102
+## [1] 0.1972643
 ## 
 ## [[5]]
-## [1] 0.1496215
+## [1] 0.1464666
 ```
 
 A detailed example can be found in the [vignette][vig].
@@ -78,11 +85,15 @@ Please report it to the [issue tracker][issues]. Please provide specific details
 
 # References
 
-Rowcliffe, J.M., Field, J., Turvey, S.T. and Carbone, C. (2008). Estimating animal density using camera traps without the need for individual recognition. J Appl Ecol. 45, 1228 â€“ 1236. [DOI: 10.1111/j.1365-2664.2008.01473.x](http://onlinelibrary.wiley.com/doi/10.1111/j.1365-2664.2008.01473.x/abstract)
+Rowcliffe, J.M., Field, J., Turvey, S.T. and Carbone, C. (2008). Estimating animal density using camera traps without the need for individual recognition. J Appl Ecol. 45, 1228 – 1236. [DOI: 10.1111/j.1365-2664.2008.01473.x](http://onlinelibrary.wiley.com/doi/10.1111/j.1365-2664.2008.01473.x/abstract)
 
 # License
 
-This package is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License, version 3, as published by the Free Software Foundation.
+This package is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License, version 3, as published by the Free Software Foundation. Please consider citing the work for which this was created and/or the package itself:
+
+**Caravaggi A**, Zaccaroni M, Riga F, Schai-Braun SC, Dick JTA, Montgomery WI, Reid N (2016) An invasive-native mammalian species replacement process captured by camera trap survey Random Encounter Models. *Remote Sensing in Ecology and Conservation*. 2: 45-58. doi: [10.1002/rse2.11](http://onlinelibrary.wiley.com/doi/10.1002/rse2.11/abstract)      
+
+**Caravaggi A**.  remBoot: An R package for Random Encounter Modelling. *Journal of Open Source Software*. 2(10). doi: [10.21105/joss.00176](http://joss.theoj.org/papers/10.21105/joss.00176)   
 
 This program is distributed in the hope that it will be useful, but without any warranty; without even the implied warranty of merchantability or fitness for a particular purpose. See the GNU General Public License for more details.
 
